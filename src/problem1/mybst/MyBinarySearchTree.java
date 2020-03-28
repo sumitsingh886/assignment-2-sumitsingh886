45286906cb529b1d4c5ed10864f6a99989a16b40
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 10:19:27 +0530

    append the preorderstring

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -16,6 +16,32 @@ public class MyBinarySearchTree {
     public MyBinarySearchTree(){
         this.root = null;
         preOrderString = new StringBuilder();
+    }
 
+    public  void  add(final int value) {
+        this.root = newService(this.root,value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot,final int addingValue) {
+        if (treeRoot == null) {
+            return new TreeNode(addingValue);
+        }
+        if (addingValue < treeRoot.getKey()) {
+            treeRoot.setLeft(addService(treeRoot.getLeft(),addingValue));
+        } else if (addingValue == treeRoot.getKey()) {
+            return treeRoot;
+        } else {
+            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
+        }
+        return treeRoot;
+    }
+    public static void preOrder(TreeNode root) {
+        if(root == null)
+        {
+            return;;
+        }
+        preOrderString.append(root.getKey()+" ");
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
     }
 }
