8dcc355d72a95a214d007d5adedd7b496c839770
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 11:32:32 +0530

    import problem3 node

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,9 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+import static problem1.mybst.MyBinarySearchTree.preOrder;
+import static problem1.mybst.MyBinarySearchTree.preOrderString;
+
 public class MyPriorityQueue {
 }
