08fd0b14a32401b7ebe2a65783ca19ca35049c5d
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 18:44:36 +0530

    final

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,5 +7,69 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollNumber;
+    private String Name;
+    private int backLogCounter;
+    private int appearingCounter;
 
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        Name = name;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCounter() {
+        return appearingCounter;
+    }
+
+    public void setAppearingCounter(int appearingCounter) {
+        this.appearingCounter = appearingCounter;
+    }
+
+    public Student(int rollNumber, String name, int backLogCounter, int appearingCounter) {
+        this.rollNumber = rollNumber;
+        Name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCounter = appearingCounter;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", Name='" + Name + '\'' +
+                '}';
+    }
+
+    public String toStrings() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", Name='" + Name + '\'' +
+                ", backLogCounter=" + backLogCounter +
+                ", appearingCounter=" + appearingCounter +
+                '}';
+    }
 }
