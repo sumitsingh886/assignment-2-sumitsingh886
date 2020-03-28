6132cd86654edb0087e5e6723a309efb82dc5bd6
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 18:52:13 +0530

    final

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -12,4 +12,53 @@ import problem5.student.Student;
 import java.util.ArrayList;
 
 public class MyCircularQueue {
+    private Node front;
+    private int size;
+
+    public MyCircularQueue() {
+        front = null;
+        size = 0;
+    }
+
+    public void enqueue(ArrayList<Student> addData) {
+        for (Student s : addData
+        ) {
+            if (s.getBackLogCounter() - s.getAppearingCounter() == 0) {
+            } else if (s.getBackLogCounter() - s.getAppearingCounter() > 0) {
+                Node temp = new Node(s);
+                if (this.front == null) {
+                    front = temp;
+                    temp.setNext(temp);
+                    size++;
+                } else {
+                    temp.setNext(this.front);
+                    Node parser = this.front;
+                    while (parser.getNext() != this.front) {
+                        parser = parser.getNext();
+                    }
+                    parser.setNext(temp);
+                    front = temp;
+                    size++;
+                }
+            } else {
+            }
+            ystem.out.println("Given Data is not Valid");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void printQueue() {
+        Node temp = this.front;
+        for (int i = 0; i < this.size; i++) {
+            if (i != this.size - 1) {
+                System.out.print(temp.getData().toStrings() + "--->");
+                temp = temp.getNext();
+            } else {
+                System.out.println(temp.getData().toStrings());
+            }
+        }
+    }
 }
