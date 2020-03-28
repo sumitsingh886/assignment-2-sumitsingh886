016c87d218779d3451c74df7b92a2ab01a6d9495
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 10:47:03 +0530

    final

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,5 +7,34 @@
 package problem1.node;
 
 public class TreeNode {
+    private int key;
+    private TreeNode left , right;
 
+    public TreeNode(int item) {
+        key = itm;
+        left = right = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public TreeNode getRight(TreeNode right) {
+        this.right = right;
+    }
 }
