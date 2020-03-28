88686bd01b9aa3b77f35f353d75498a0efa530d8
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 18:46:50 +0530

    final

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -9,4 +9,27 @@ package problem5.node;
 import problem5.student.Student;
 
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData() {
+        return student;
+    }
+
+    public void setData(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
