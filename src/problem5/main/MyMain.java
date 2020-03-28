924e54ff02ce46ca7fe426514caabc93f022a6c9
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 18:48:32 +0530

    create array list

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -9,5 +9,10 @@ package problem5.main;
 import problem5.circularqueue.MyCircularQueue;
 import problem5.student.Student;
 
+import java.util.ArrayList;
+
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<Student>();
+    }
 }
