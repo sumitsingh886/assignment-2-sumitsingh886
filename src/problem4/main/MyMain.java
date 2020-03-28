f31680755b5f5decd0f2b1bca8e1356736cd9d1b
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 11:14:03 +0530

    add value in tree

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -10,4 +10,22 @@ import problem1.mybst.MyBinarySearchTree;
 import problem4.myqueue.MyQueue;
 
 public class MyMain {
+    public static void  main(String[] args){
+        MyBimarySearchTree tree = new MyBinarySearchTree();
+        tree.add(51);
+        tree.add(39);
+        tree.add(31);
+        tree.add(54);
+        tree.add(92);
+        tree.add(42);
+        tree.add(21);
+        tree.add(10);
+        tree.add(26);
+        tree.add(52);
+        tree.add(36);
+        tree.add(47);
+        tree.add(82);
+        tree.add(5);
+        tree.add(62);
+    }
 }
