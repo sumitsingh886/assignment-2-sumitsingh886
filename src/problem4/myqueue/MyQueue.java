f0c48b5ee76f04007f69c87195378d0dbdc7a5c8
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 15:38:24 +0530

    init

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -12,4 +12,41 @@ import static problem1.mybst.MyBinarySearchTree.preOrder;
 import static problem1.mybst.MyBinarySearchTree.preOrderString;
 
 public class MyQueue {
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+    public void enqueue(TreeNode root) {
+        preOrder(root);
+        String[] data = preOrderString.toString().split(" ");
+        for(int i;i<data.length;i++){
+            Node node = new Node(Integer.parseInt(data[i]));
+            if(front == null) {
+                rear = node;
+                front = node;
+                size++;
+            } else {
+                rear.setNext(node);
+                rear = node;
+                size++;
+            }
+        }
+    }
+    public void printQueue(){
+        Node temp = this.front.getNext();
+        for(int i;i<this.size-1;i++){
+            if(i!=this.size-2){
+                System.out.println(temp.getData()+"--->");
+                temp = temp.getNext();
+            }
+            else {
+                System.out.println(temp.getData());
+            }
+        }
+    }
 }
