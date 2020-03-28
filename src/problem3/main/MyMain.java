82e7097ba249351ca8d8c46a86bd48388bb31e58
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 11:27:35 +0530

    adding 5 student

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -11,4 +11,12 @@ import problem3.node.Node;
 import problem5.student.Student;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        myPriorityQueue.enqueue(new Node(new Student(15,"wdad")));
+        myPriorityQueue.enqueue(new Node(new Student(500,"wdad")));
+        myPriorityQueue.enqueue(new Node(new Student(50,"wdad")));
+        myPriorityQueue.enqueue(new Node(new Student(11,"wdad")));
+        myPriorityQueue.enqueue(new Node(new Student(55,"wdad")));
+    }
 }
