35a11d520cf14b4b9663c85ba619ea688b428cb4
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 11:28:07 +0530

    print queue

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -18,5 +18,6 @@ public class MyMain {
         myPriorityQueue.enqueue(new Node(new Student(50,"wdad")));
         myPriorityQueue.enqueue(new Node(new Student(11,"wdad")));
         myPriorityQueue.enqueue(new Node(new Student(55,"wdad")));
+        myPriorityQueue.printQueue();
     }
 }
