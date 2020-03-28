892ca147a899d318ac0808f09d06d6d715515b0a
sumitsingh886 <sk50060093@gmail.com>
2020-03-28 18:49:00 +0530

    add student detail

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -14,5 +14,16 @@ import java.util.ArrayList;
 public class MyMain {
     public static void main(String[] args) {
         ArrayList<Student> studentList = new ArrayList<Student>();
+        studentList.add(new Student(1, "A", 1, 0));
+        studentList.add(new Student(2, "B", 2, 2));
+        studentList.add(new Student(3, "C", 1, 1));
+        studentList.add(new Student(4, "D", 2, 2));
+        studentList.add(new Student(5, "E", 0, 0));
+        studentList.add(new Student(6, "F", 2, 1));
+        studentList.add(new Student(7, "G", 1, 1));
+        studentList.add(new Student(8, "H", 2, 1));
+        studentList.add(new Student(9, "I", 0, 0));
+        studentList.add(new Student(10, "J", 0, 0));
+        studentList.add(new Student(11, "K", 2, 1));
     }
 }
